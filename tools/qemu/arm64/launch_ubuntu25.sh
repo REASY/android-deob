6aa0315d99999d495330d020f86f33f7fa236f94
Artavazd Balaian <reasyu@gmail.com>
2025-10-04 22:58:53 +0800

    fix: Better preparation script with cloud-init

--- tools/qemu/arm64/launch_ubuntu25.sh
+++ tools/qemu/arm64/launch_ubuntu25.sh
@@ -8,7 +8,7 @@ qemu-system-aarch64 \
  -nographic \
  -drive if=pflash,format=raw,file=efi.img,readonly=on \
  -drive if=pflash,format=raw,file=varstore.img \
- -drive if=none,file=plucky-server-cloudimg-arm64.img,id=hd0 \
+ -drive if=none,file=ubuntu-25.04-server-cloudimg-arm64.img,id=hd0 \
  -drive file=user-data.img,format=raw,id=cloud \
  -device virtio-blk-device,drive=hd0 \
  -net user,hostfwd=tcp::30022-:22,hostfwd=tcp::5551-:5555 \
--- tools/qemu/arm64/prepare.sh
+++ tools/qemu/arm64/prepare.sh
@@ -2,17 +2,18 @@
 
 set -e
 
+# Download the image
+wget "https://cloud-images.ubuntu.com/releases/plucky/release/ubuntu-25.04-server-cloudimg-arm64.img"
+
+#cp -f /home/user/Downloads/ubuntu-25.04-server-cloudimg-arm64.img ubuntu-25.04-server-cloudimg-arm64.img
+
+# Resize it to 50G
+qemu-img resize ubuntu-25.04-server-cloudimg-arm64.img 50G
+
 truncate -s 64m varstore.img
 
 truncate -s 64m efi.img
 dd if=/usr/share/qemu-efi-aarch64/QEMU_EFI.fd of=efi.img conv=notrunc
 
-cat >user-data <<EOF
-#cloud-config
-password: 1
-chpasswd: { expire: False }
-ssh_pwauth: True
-EOF
-
 # sudo apt-get install cloud-image-utils
-cloud-localds user-data.img user-data
+cloud-localds user-data.img cloud-init.yaml
